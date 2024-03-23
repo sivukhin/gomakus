c3881f2f51d74f0cb7cc6ae14e04c1348947fb78
Nikita Sivukhin <sivukhin@sharechat.co>
2024-03-23 20:36:23 +0400

    add simple CLI args

--- main.go
+++ main.go
@@ -1,20 +1,59 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"go/ast"
 	"log"
+	"os"
+	"path/filepath"
 
 	"golang.org/x/tools/go/packages"
 
 	"gomakus/src"
 )
 
+func reportWarning(format string, analysisPath string, fileName, funcName string, line int) {
+	if format == "github" {
+		relativePath, _ := filepath.Rel(analysisPath, fileName)
+		fmt.Printf("::warning file=%v,line=%v::%v\n", relativePath, line, "potential append overwrite found")
+	} else {
+		log.Printf(
+			"potential append overwrite found: func=[%v], file=[%v], line=[%v]",
+			funcName,
+			fileName,
+			line,
+		)
+	}
+}
+
 func main() {
+	modulePath := flag.String("path", "", "path to the module root (with go.mod file)")
+	reportFormat := flag.String("format", "log", "reporting type (github | log)")
+	flag.Parse()
+
+	var analysisPath string
+	var err error
+	if *modulePath == "" {
+		analysisPath, err = os.Getwd()
+		if err != nil {
+			fmt.Printf("unable to get working directory: %v\n", err)
+			flag.Usage()
+			os.Exit(1)
+		}
+	} else {
+		analysisPath, err = filepath.Abs(*modulePath)
+		if err != nil {
+			fmt.Printf("unable to expand path '%v' to absolute: %v\n", *modulePath, err)
+			flag.Usage()
+			os.Exit(1)
+		}
+	}
+
 	cfg := &packages.Config{
 		Mode:  packages.NeedSyntax | packages.NeedFiles | packages.NeedTypes,
 		Tests: false,
-		Dir:   ".",
+		Dir:   analysisPath,
 	}
 
 	pkgs, err := packages.Load(cfg, "./...")
@@ -41,18 +80,9 @@ func main() {
 				for _, warning := range warnings {
 					pos, ok := execution.SourceCodeReferences.References[warning.ExecutionPoint]
 					if ok {
-						log.Printf(
-							"potential append overwrite found in function: func=[%v], file=[%v], line=[%v]",
-							funcDecl.Name,
-							pkg.Fset.Position(pos).Filename,
-							pkg.Fset.Position(pos).Line,
-						)
+						reportWarning(*reportFormat, analysisPath, pkg.Fset.Position(pos).Filename, funcDecl.Name.Name, pkg.Fset.Position(pos).Line)
 					} else {
-						log.Printf(
-							"potential append overwrite found: func=[%v], file=[%v]",
-							funcDecl.Name,
-							pkg.Fset.Position(funcDecl.Pos()).Filename,
-						)
+						reportWarning(*reportFormat, analysisPath, pkg.Fset.Position(pos).Filename, funcDecl.Name.Name, pkg.Fset.Position(funcDecl.Pos()).Line)
 					}
 				}
 				return true
